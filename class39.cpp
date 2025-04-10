48829d5413f7b2d16d01ee9fbd32664de1f292c9
Gaurisingh-064064 <24csu064@ncuindia.edu>
2025-04-10 23:13:31 +0530

    Add files via upload

--- class39.cpp
+++ class39.cpp
@@ -10,12 +10,12 @@ class Students{
     Students(string name,int rollno,float cgpa,int nocour);
     Students(const Students &r);
     ~Students(){
-     cout<<"destructor called";
+     cout<<"\n destructor called";
     }
-    
+    void display();
 };
 Students::Students(){
-    cout<<"deafult constructor ";
+    cout<<"\n deafult constructor ";
 }
 Students::Students(string name,int rollno,float cgpa,int nocour){
     cout<<" parameterized constructor ";
@@ -30,6 +30,15 @@ Students::Students(const Students &r){
     cgpa=cgpa;
     nocour=nocour;
 }
+void Students::display(){
+    cout<<"\n name is "<<name;
+    cout<<"\n rollno is "<<rollno;
+    cout<<"\n cgpa is "<<cgpa;
+    cout<<"\n nocour is "<<nocour;
+}
 int main(){
     Students s1("gauri",23,4.5,8),s2("abhiraj",34,5.6,7),s3(s1);
+    s1.display();
+    s2.display();
+    s3.display();
 }
